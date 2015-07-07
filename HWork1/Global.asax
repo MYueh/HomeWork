62855224b1c4a4e0894dd7aed022811d956892cd
MYueh <gto_ta@hotmail.com>
2015-07-07 23:28:43 +0800

    Initail Commit

---
+++ HWork1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HWork1.MvcApplication" Language="C#" %>
---
+++ HWork1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HWork1.Startup))]
+namespace HWork1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
