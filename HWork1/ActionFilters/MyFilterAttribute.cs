cbd6c28b0ccf457f084db432acff518e68ca72fa
MYueh <gto_ta@hotmail.com>
2015-07-24 21:57:03 +0800

    ActionFilter套用在AFController/Index ([MyFilter])

---
+++ HWork1/ActionFilters/MyFilterAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Web.Mvc;
+
+namespace HWork1.ActionFilters
+{
+    public class MyFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            System.IO.File.AppendAllText(@"D:\MyFilter.log", "#1\n");
+            base.OnActionExecuting(filterContext);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            System.IO.File.AppendAllText(@"D:\MyFilter.log", "#3\n");            
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
---
+++ HWork1/Controllers/AFController.cs
@@ -0,0 +1,20 @@
+﻿using HWork1.ActionFilters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HWork1.Controllers
+{
+    public class AFController : BaseController   //Action Filter
+    {
+        // GET: AF
+        [MyFilter]
+        public ActionResult Index()
+        {
+            System.IO.File.AppendAllText(@"D:\MyFilter.log", "#2\n");
+            return Content("Done");
+        }
+    }
+}
